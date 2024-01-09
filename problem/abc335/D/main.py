51c5433bbdec0a475419de15127d5d329fe6fe10
pivote1214 <yuta.m.12.1214@gmail.com>
2024-01-09 18:55:05 +0900

    abc335

--- problem/abc335/D/main.py
+++ problem/abc335/D/main.py
@@ -3,6 +3,22 @@ import sys
 
 
 def solve(N: int):
+    ans = [[None] * N for _ in range(N)]
+    x, y = 0, 0
+    dx, dy = 1, 0
+    for i in range(1, N * N):
+        ans[y][x] = i
+        nx, ny = x + dx, y + dy
+        if nx < 0 or N <= nx or ny < 0 or N <= ny or ans[ny][nx] is not None:
+            dx, dy = -dy, dx
+            nx, ny = x + dx, y + dy
+
+        x, y = nx, ny
+
+    ans[y][x] = 'T'
+
+    for row in ans:
+        print(*row)
 
 
 def main():
